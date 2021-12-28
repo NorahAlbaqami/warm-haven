aa317aafff41c0c385985661ecdb38fc24ec253a
Norah <norayourstar@gmail.com>
2021-12-28 16:49:27 +0300

    screens design not completed

--- app/src/main/java/com/norah/albaqami/warmhaven/user/data/LogInActivity.kt
+++ app/src/main/java/com/norah/albaqami/warmhaven/user/data/LogInActivity.kt
@@ -1,6 +1,6 @@
 package com.norah.albaqami.warmhaven.user.data
 
-import android.app.Activity
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,15 +9,12 @@ import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
 import com.norah.albaqami.warmhaven.R
 
 class LogInActivity : AppCompatActivity() {
-    companion object{
-        private const val RC_SIGN =1992
-    }
+
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
     ) { res ->
@@ -38,7 +35,6 @@ class LogInActivity : AppCompatActivity() {
        // Create and launch sign-in intent
        val signInIntent = AuthUI.getInstance()
            .createSignInIntentBuilder()
-
            .setAvailableProviders(providers)
            .setIsSmartLockEnabled(false)
            .setTheme(R.style.LoginUi)
@@ -54,7 +50,6 @@ class LogInActivity : AppCompatActivity() {
         val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-                Log.d("TAG", "${user?.displayName}")
                startActivity(Intent(this,ProfileActivity::class.java))
             }else{
                 if (response == null){
