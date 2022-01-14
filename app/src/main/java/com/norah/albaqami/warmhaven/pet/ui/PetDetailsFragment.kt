778054e7444e85cdf7f1df886247ae2045e585c2
Norah <norayourstar@gmail.com>
2022-01-14 14:36:27 +0300

    fix errors

--- app/src/main/java/com/norah/albaqami/warmhaven/pet/ui/PetDetailsFragment.kt
+++ app/src/main/java/com/norah/albaqami/warmhaven/pet/ui/PetDetailsFragment.kt
@@ -1,5 +1,9 @@
 package com.norah.albaqami.warmhaven.pet.ui
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -7,9 +11,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.viewModels
-import com.bumptech.glide.Glide
 import com.google.firebase.database.FirebaseDatabase
 import com.norah.albaqami.warmhaven.binding.bindImage
 import com.norah.albaqami.warmhaven.databinding.FragmentPetDetailsBinding
@@ -25,8 +31,6 @@ class PetDetailsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments.let {
-//            viewModel.setPetDetails(it?.getInt("petId")!!)
-//            Log.d("meme", "onCreateView:${viewModel.setPetDetails(it?.getInt("petIndex")!!)} ")
             id = it?.getString("petId").toString()
 
         }
@@ -47,26 +51,44 @@ class PetDetailsFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        getD(id)
+        getDetails(id)
     }
 
-    private fun getD(arg: String) {
-
-
+    private fun getDetails(arg: String) {
         val db = FirebaseDatabase.getInstance()
         val mRef = db.getReference("pet").child(arg)
-        Log.e("TAG", "onViewCreated: $mRef")
+
         mRef.get().addOnCompleteListener { DataSnapshot1 ->
             val petdetails = DataSnapshot1.result.getValue(PetItem::class.java)
-            binding.petDescription.text = petdetails?.description.toString() ?: ""
-            bindImage(binding.petPictureDetail,petdetails?.image)
+            binding.petDescription.text = petdetails?.description.toString()
+            bindImage(binding.petPictureDetail, petdetails?.image)
+            binding.petLocation.text = petdetails?.location.toString()
+            binding.petNameDetail.text = petdetails?.name.toString()
+            binding.callBtn.setOnClickListener {
+                var phoneNumber = petdetails?.phone.toString().trim()
+                if(ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        android.Manifest.permission.CALL_PHONE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Please Grand Permission for phone call ",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    val intent =
+                        Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(phoneNumber)))
+                    startActivity(intent)
+                }
+
+            }
             binding.executePendingBindings()
 //           var e = (DataSnapshot1.result.value)
 //               toObject(PetItem::class.java)
-
-            Log.e("tata", "onViewCreated: result ${petdetails}")
-            Log.e("tata", "onViewCreated:  DataSnapshot1 ${petdetails}")
+//
+//            Log.e("tata", "onViewCreated: result ${petdetails}")
+//            Log.e("tata", "onViewCreated:  DataSnapshot1 ${petdetails}")
         }
 
     }
